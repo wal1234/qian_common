4de0d482843a5fb607ba2f36b9c1be499c8088f3
zbq <zhaoboqi6@gmail.com>
2025-06-03 17:58:55 +0800

    公用返回体以及公用异常枚举优化

---
+++ commons/src/main/java/com/qian/common/utils/uuid/IdUtils.java
@@ -0,0 +1,44 @@
+package com.qian.common.utils.uuid;
+
+import java.util.UUID;
+
+/**
+ * ID生成工具类
+ */
+public class IdUtils {
+    /**
+     * 获取随机UUID
+     *
+     * @return 随机UUID
+     */
+    public static String randomUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 简化的UUID，去掉了横线
+     *
+     * @return 简化的UUID，去掉了横线
+     */
+    public static String simpleUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
+     * 获取随机UUID，使用性能更好的ThreadLocalRandom生成UUID
+     *
+     * @return 随机UUID
+     */
+    public static String fastUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 简化的UUID，去掉了横线，使用性能更好的ThreadLocalRandom生成UUID
+     *
+     * @return 简化的UUID，去掉了横线
+     */
+    public static String fastSimpleUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+} 
